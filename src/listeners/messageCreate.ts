85b5a79b1bb5aae31eb749ca3c522651ee39b0a5
ZenShibata <zen@ayato.life>
2022-06-27 20:22:00 +0700

    feat: delete unwanted messages

---
+++ src/listeners/messageCreate.ts
@@ -0,0 +1,16 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Listener } from "@sapphire/framework";
+import { Message } from "discord.js";
+import { Channels } from "../constants";
+
+@ApplyOptions<Listener.Options>({
+    event: "messageCreate"
+})
+export class MessageCreateListener extends Listener {
+    // eslint-disable-next-line class-methods-use-this
+    public async run(message: Message): Promise<any> {
+        if (!message.member?.permissions.has("MANAGE_GUILD") && message.channelId === Channels.SUGGESTION) {
+            if (message.deletable) await message.delete();
+        }
+    }
+}
