2a1231b533558291f1e1afb808be27430fe5fc5f
ZenShibata <zen@ayato.life>
2022-09-24 18:47:50 +0700

    feat: add autorole

---
+++ src/listeners/guildMemberUpdate.ts
@@ -0,0 +1,20 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Listener } from "@sapphire/framework";
+import { GuildMember, Message } from "discord.js";
+import { autoRole, Channels, Guild } from "../constants";
+
+@ApplyOptions<Listener.Options>({
+    event: "guildMemberUpdate"
+})
+export class GuildMemberUpdateListener extends Listener {
+    // eslint-disable-next-line class-methods-use-this
+    public async run(oldMember: GuildMember, newMember: GuildMember): Promise<any> {
+        if (newMember.guild.id === Guild.PRIMARY) {
+            if (oldMember.pending && !newMember.pending) {
+                for (const role of autoRole) {
+                    await newMember.roles.add(role, "Auto-role after accepting rules screening");
+                }
+            }
+        }
+    }
+}
