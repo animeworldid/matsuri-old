8cbd96506f6da203222bdc8b2aef2592dee8bd33
Zen <45705890+ZenShibata@users.noreply.github.com>
2022-09-24 18:55:53 +0700

    feat: add autorole (#1)
    * feat: add autorole
    
    * fix: solve eslint
    
    * feat: add delay to avoid ratelimit

---
+++ src/listeners/guildMemberUpdate.ts
@@ -0,0 +1,22 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Listener } from "@sapphire/framework";
+import { GuildMember } from "discord.js";
+import { autoRole, Guild } from "../constants";
+
+@ApplyOptions<Listener.Options>({
+    event: "guildMemberUpdate"
+})
+export class GuildMemberUpdateListener extends Listener {
+    // eslint-disable-next-line class-methods-use-this
+    public async run(oldMember: GuildMember, newMember: GuildMember): Promise<any> {
+        if (newMember.guild.id === Guild.PRIMARY) {
+            if (oldMember.pending && !newMember.pending) {
+                for (const role of autoRole) {
+                    if (newMember.roles.cache.has(role)) continue;
+                    await new Promise(res => setTimeout(() => res(true), 1_500));
+                    await newMember.roles.add(role, "Auto-role after accepting rules screening");
+                }
+            }
+        }
+    }
+}
