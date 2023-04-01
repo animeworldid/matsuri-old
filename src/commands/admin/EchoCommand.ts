581162744bba80f4bffe02e4fd70a99a8c9a3f54
Plyrs1 <me@plyrs.eu.org>
2023-04-02 00:46:40 +0700

    feat: echo reply to message (#35)

--- src/commands/admin/EchoCommand.ts
+++ src/commands/admin/EchoCommand.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-eval */
 import { Command, ApplicationCommandRegistry, RegisterBehavior, CommandOptionsRunTypeEnum, Result } from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
-import { ApplicationCommandOptionType, TextChannel } from "discord.js";
+import { APIEmbedField, ApplicationCommandOptionType, EmbedBuilder, TextChannel } from "discord.js";
 import { guildsToRegister } from "../../config";
-
+import { Channels } from "../../constants";
 @ApplyOptions<Command.Options>({
     aliases: ["say"],
     name: "echo",
@@ -32,6 +32,11 @@ export class ExecCommand extends Command {
                     name: "channel",
                     type: ApplicationCommandOptionType.Channel,
                     description: "Channel you want to send the message to"
+                },
+                {
+                    name: "reply_to",
+                    type: ApplicationCommandOptionType.String,
+                    description: "Message id you want to reply"
                 }
             ]
         }, {
@@ -45,9 +50,28 @@ export class ExecCommand extends Command {
     public async chatInputRun(interaction: Command.ChatInputCommandInteraction): Promise<any> {
         const input = interaction.options.getString("input", true);
         const channel = interaction.options.getChannel("channel")?.id ?? interaction.channelId;
+        const replyToId = interaction.options.getString("reply_to");
         const textChannel = await interaction.client.channels.fetch(channel) as TextChannel;
-        const isSent = (await Result.fromAsync(textChannel.send(input))).isOk();
+        const logChannel = await interaction.client.channels.fetch(Channels.ECHO_LOG) as TextChannel;
+        const fields: APIEmbedField[] = [];
+        fields.push({ name: "#️⃣ Channel", value: `<#${textChannel.id}>`, inline: true });
+        let isSent = false;
+        if (replyToId) {
+            const replyMessage = await textChannel.messages.fetch(replyToId);
+            isSent = (await Result.fromAsync(replyMessage.reply(input))).isOk();
+            fields.push({ name: "↩ Reply To", value: `<@${replyMessage.author.id}>`, inline: true });
+            fields.push({ name: "✉ Content", value: replyMessage.content });
+        } else {
+            isSent = (await Result.fromAsync(textChannel.send(input))).isOk();
+        }
+        fields.push({ name: "📨 Message", value: input });
         if (!isSent) return interaction.reply({ content: "Failed to send message. No perms, probably?", ephemeral: true });
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: "🔊 Echo", iconURL: this.container.client.user!.displayAvatarURL() })
+            .addFields(fields)
+            .setFooter({ text: `Requested by: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() })
+            .setTimestamp();
+        await logChannel.send({ embeds: [embed] });
         return interaction.reply({ content: "Message sent!", ephemeral: true });
     }
 }
